a6eb04b89735ba6323a74a7f951702d3af53b91b
s0o0bn <0921soobin@gmail.com>
2024-08-15 00:24:08 +0900

    :sparkles: API 결과로 BoardGame 조회 후 DTO 매핑 기능 추가

--- src/main/java/com/yeoboge/recommendation/application/personal/AIRecommendService.java
+++ src/main/java/com/yeoboge/recommendation/application/personal/AIRecommendService.java
@@ -1,5 +1,7 @@
 package com.yeoboge.recommendation.application.personal;
 
+import com.yeoboge.recommendation.core.boardgame.BoardGame;
+import com.yeoboge.recommendation.core.boardgame.BoardGameRepository;
 import com.yeoboge.recommendation.core.boardgame.dto.BoardGameThumbnailDto;
 import com.yeoboge.recommendation.infra.util.RestClientUtil;
 import lombok.RequiredArgsConstructor;
@@ -14,10 +16,12 @@ import java.util.List;
 public class AIRecommendService implements RecommendService {
     public static final String BASE_URL = "http://localhost:9090/recommends";
 
+    private final BoardGameRepository repository;
+
     @Override
     public List<BoardGameThumbnailDto> getRecommendedBoardGames() {
         List<Long> recommendedIds = getRecommendedBoardGameIds();
-        return null;
+        return mapThumbnailFromBoardGame(recommendedIds);
     }
 
     private List<Long> getRecommendedBoardGameIds() {
@@ -28,6 +32,11 @@ public class AIRecommendService implements RecommendService {
                 .retrieve()
                 .body(Response.class);
         return result.result;
+    }
+
+    private List<BoardGameThumbnailDto> mapThumbnailFromBoardGame(List<Long> ids) {
+        List<BoardGame> boardGames = repository.findBoardGamesByIdIsIn(ids);
+        return boardGames.stream().map(BoardGameThumbnailDto::from).toList();
     }
 
     private record Request(int user_id, int genre_id) {}
--- src/main/java/com/yeoboge/recommendation/core/boardgame/BoardGameRepository.java
+++ src/main/java/com/yeoboge/recommendation/core/boardgame/BoardGameRepository.java
@@ -3,6 +3,9 @@ package com.yeoboge.recommendation.core.boardgame;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BoardGameRepository extends JpaRepository<BoardGame, Long> {
+    List<BoardGame> findBoardGamesByIdIsIn(List<Long> ids);
 }
